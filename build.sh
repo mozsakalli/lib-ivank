03a2fa2fe475d81e2dcce47bb16c2332a2e062e1
spookerton <spkrtn@pm.me>
2021-09-09 15:57:38 +0100

    squish build flow, sources -> src, game -> doc
    collects things together more coherently and adds github sugar

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+VERSION=1.2.0
+
+cat src/geom/Point.js \
+    src/geom/Rectangle.js \
+    src/geom/Transform.js \
+    > obj/geom.js
+
+cat src/events/EventDispatcher.js \
+    src/events/Event.js \
+    src/events/MouseEvent.js \
+    src/events/TouchEvent.js \
+    src/events/KeyboardEvent.js \
+    > obj/events.js
+
+cat src/display/BlendMode.js \
+    src/display/DisplayObject.js \
+    src/display/InteractiveObject.js \
+    src/display/DisplayObjectContainer.js \
+    src/display/BitmapData.js \
+    src/display/Bitmap.js \
+    src/display/Stage.js \
+    src/display/Graphics.js \
+    src/display/Sprite.js \
+    > obj/display.js
+
+cat src/text/TextFormatAlign.js \
+    src/text/TextFormat.js \
+    src/text/TextField.js \
+    > obj/text.js
+
+cat src/Utils.js \
+    obj/geom.js \
+    obj/events.js \
+    obj/display.js \
+    obj/text.js \
+    > obj/lib/ivank-${VERSION}.js
--- game/join.sh
+++
@@ -1,2 +0,0 @@
-cat Resizable.js  MarginCont.js RectCont.js MainMenu.js LevelSelect.js GameControl.js Main.js  BDFac.js \
- > game.js
